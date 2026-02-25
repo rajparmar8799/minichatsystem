0ce11df1073cd1546e791d4c5a759b164ae6bbf8
raj <prmrraj.1911@gmail.com>
2026-02-25 13:59:41 +0530

    completed mini chat system

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const Chat = require("./models/chat.js");
+const methodOverride = require ('method-override');
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride('_method'));
 
 main().catch(err => {
     console.log(err);
@@ -24,9 +27,53 @@ app.get('/chats',async (req,res)=>{
     res.render("index.ejs",{chats});
 })
 
+app.post('/chats',(req,res)=>{
+    let {f ,m, t} = req.body;
+    let newChat = new Chat({
+        from : f,
+        to: t,
+        message : m,
+        created_at : new Date(),
+    });
+    
+    newChat.save().then(()=>{
+        res.redirect('/chats');
+    }).catch(err => {
+        console.log(err);
+    });          
+    
+})
+
+app.put('/chats/:id',(req,res)=>{
+    let {msg : editedMessage} = req.body;
+    let {id} = req.params;
+    Chat.findByIdAndUpdate(id,{
+        message : editedMessage,
+    },{
+        runValidators : true,
+        new : true
+    }).then(()=>{
+        res.redirect('/chats');
+    }).catch(err => {
+        console.log(err);
+    });
+})
+
+app.get('/chats/:id/edit',async(req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    // console.log(chatData);
+    res.render("edit.ejs",{chat});
+})
+
+app.get('/chats/new',(req,res)=>{
+    console.log("opened form for new chat");
+    res.render("newchat.ejs");
+})
+
 app.get('/',(req,res)=>{
     console.log("root is workin");
-    res.send("Server Working");
+    res.render("home.ejs");
 })
 
 app.listen(8080,()=>{
